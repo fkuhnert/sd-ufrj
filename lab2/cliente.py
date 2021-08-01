9f37548f98d93a74d348bdf2bd2ff4830d43c265
fkuhnert <fvk98@yahoo.com.br>
2021-08-01 19:22:57 -0300

    Added processing and error handling

--- lab2/cliente.py
+++ lab2/cliente.py
@@ -9,11 +9,35 @@ sock = socket.socket() # default: socket.AF_INET, socket.SOCK_STREAM
 # conecta-se com o par passivo
 sock.connect((HOST, PORTA)) 
 
-print("Digite as mensagens a serem enviadas, ou digite \"exit\" para sair")
-text = ""
 while True:
-    text = input("# ")
-    if len(text):               # nao envia mensagem vazia
-        sock.send(text.encode(encoding='utf-8'))
-    if text == "exit": break
-sock.close() 
+    arquivo = ""
+    # Não permite o usuário de tentar enviar uma string vazia
+    while arquivo == "":
+        arquivo = input("Digite o nome do arquivo de texto (ou \"exit\" para sair): ")
+    
+    palavra = ""
+    if arquivo != "exit":
+        while palavra == "":
+            palavra = input("Digite a palavra a ser buscada: ")
+        print() # Print vazio para melhorar visibilidade
+    
+    # Se o nome do arquivo for "exit", fecha a conexão e encerra o programa
+    if arquivo == "exit":
+        sock.close()
+        break
+
+    else:
+        # Envio da mensagem ao servidor
+        mensagem = f"{arquivo}/{palavra}".encode('utf-8')
+        sock.send(mensagem)
+
+        # Recebimento da resposta do servidor
+        response = sock.recv(1024).decode('utf-8')
+
+        # Tratamento e exibição da mensagem ao usuário
+        if "erro" in response:
+            print("O arquivo solicitado não foi encontrado no servidor.")
+        else:
+            vez_str = "vez" if int(response) == 1 else "vezes"
+            print(f"A palavra \"{palavra}\" foi encontrada {response} {vez_str}.")
+        print()
--- lab2/servidor.py
+++ lab2/servidor.py
@@ -18,11 +18,42 @@ while True:
     # Se nenhuma conexão existe, estabelece uma nova
     if novoSock is None:
         novoSock, endereco = sock.accept() # retorna um novo socket e o endereco do par conectado
-        print ('Conectado com: ', endereco)
+        print (f"Conectado com {endereco}")
 
-    msg = novoSock.recv(1024).decode('utf-8')
-    print(msg)
-    if msg == "exit":
+    msg = novoSock.recv(1024).decode("utf-8")
+    if msg:
+        print(f"Mensagem recebida: {msg}")
+
+    # Quando o cliente encerra conexão, o loop deve ser interrompido
+    if not msg:
         novoSock.close()
         novoSock = None
         print (f"Conexão com {endereco} fechada")
+        continue
+
+    # Separa a mensagem em arquivo a ser aberto e palavra a ser buscada
+    arquivo, palavra = msg.split("/")
+    print(f"Arquivo: {arquivo}")
+    print(f"Palavra: {palavra}")
+
+    # Print vazio para melhorar visibilidade
+    print()
+
+    try:
+        total = 0
+        file = open(arquivo, "r")
+        print(f"Arquivo {arquivo} aberto corretamente.")
+
+        # Para cada linha no arquivo, conta quantas vezes a palvra aparece na linha
+        for line in file:
+            total += line.count(palavra)
+        print(f"Encontrei a palavra {palavra} {total} vez(es).")
+        
+        # Transforma em texto e envia de volta ao cliente
+        novoSock.send(str(total).encode("utf-8"))
+
+    # Quando o arquivo não é encontrado, é disparada uma exceção
+    except:
+        print(f"Falha ao abrir o arquivo {arquivo}.")
+        novoSock.send("erro ao abrir o arquivo".encode('utf-8'))
+    print()
